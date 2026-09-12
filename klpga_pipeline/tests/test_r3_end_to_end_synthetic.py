ed4deb07de27df7c038d250533ef079c624ffb1b
mymedia518-alt <mymedia518@gmail.com>
2026-09-12 18:03:41 +0900

    Update synthetic R3 WD contract assertion

--- klpga_pipeline/tests/test_r3_end_to_end_synthetic.py
+++ klpga_pipeline/tests/test_r3_end_to_end_synthetic.py
@@ -248,7 +248,7 @@ def test_full_chain_publishes_with_wd_player_excluded_from_main_table(context, t
 
     rows_html = re.findall(r"<tr data-player-id='([^']+)'>", real_html.split("<tbody>", 1)[1].split("</tbody>", 1)[0])
     assert set(rows_html) == {"e1", "e2", "e3"}  # explicit WD is rendered with status
-    assert "가상선수삼" not in real_html
+    assert "status-badge'>WD</span>" in real_html
 
     report = r3_publication_gate.evaluate_r3_publication_gate(GAME_CODE, {
         "official_source_verified": (r3_publication_gate.PASS, "x"),
